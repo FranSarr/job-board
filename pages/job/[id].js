196756b4d79ed15c289845943a1570228a777d61
fsarrias <fsarrias@kolab.es>
2022-06-11 20:02:42 +0200

    I add the list of applications

--- pages/job/[id].js
+++ pages/job/[id].js
@@ -1,8 +1,10 @@
-import { getJob } from 'lib/data'
 import prisma from 'lib/prisma'
 import Link from 'next/link'
+import { getJob, alreadyApplied } from 'lib/data.js'
+import { getSession } from 'next-auth/react'
 
-export default function Job({job}) {
+
+export default function Job({job,applied}) {
   
     return (
 
@@ -21,13 +23,32 @@ export default function Job({job}) {
         <div className='pl-16 pr-16 -mt-6'>
           <p className='text-base font-normal mt-3'>{job.description}</p>
 
-          <div className='mt-20 flex justify-center '>
+       {/*   <div className='mt-20 flex justify-center '>
         <Link href={`/job/${job.id}/apply`}>
         <button className=' border  px-8 py-2 mt-0  font-bold rounded-full bg-black text-white '>
          Apply to this job
           </button>
          </Link>
           </div>
+      */}
+
+        {applied ? (
+           <div className='mt-20 flex justify-center '>
+            <Link href={`/dashboard`}>
+            <button className=' border  px-8 py-2 mt-0  font-bold rounded-full bg-black text-white '>
+              You already applied!
+            </button>
+           </Link>
+           </div>
+            ) : (
+           <div className='mt-20 flex justify-center '>
+             <Link href={`/job/${job.id}/apply`}>
+             <button className=' border  px-8 py-2 mt-0  font-bold rounded-full bg-black text-white '>
+            Apply to this job
+            </button>
+             </Link>
+            </div>
+          )}
 
 
 
@@ -56,12 +77,21 @@ export default function Job({job}) {
   }
   
   export async function getServerSideProps(context) {
+    const session = await getSession(context)
+
     let job = await getJob(context.params.id, prisma)
     job = JSON.parse(JSON.stringify(job))
+
+    const applied = await alreadyApplied(
+      session.user.id,
+      context.params.id,
+      prisma
+    )
   
     return {
       props: {
         job,
+        applied,
       },
     }
   }
